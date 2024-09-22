41f9998ee69aef9f40002632d405b80d739be55c
Rishav Das <rishavdas27602@gmail.com>
2024-09-22 09:54:06 +0530

    Assignment Solved

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
-}
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+//        obj.name = "Rishav";
+//        C:\document\Acciojob_Backend\Spring_September\encapsulation-d19rsabhah\src\main\java\com\driver\Main.java:6:12
+//        java: name has private access in com.driver.RWOnly
+        obj.setName("Rishav das");
+        System.out.println("Name: " + obj.getName());
+    }
+}g
